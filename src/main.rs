5464285d4f3f3ebd292453e09a37ee0e83a02efe
Lucas Balieiro <lucasbalieirom@hotmail.com>
2025-03-05 21:38:35 -0400

    feat: implements echo server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,36 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    println!("Server started at localhost:8080");
+
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+            println!("Connection received from {:?}", addr);
+            let mut reader = BufReader::new(reader);
+
+            loop {
+                let mut line = String::new();
+
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+
+                if bytes_read == 0 {
+                    break;
+                }
+
+                println!("GOT={}", line);
+
+                writer.write_all(&line.as_bytes()).await.unwrap();
+
+                line.clear();
+            }
+        });
+    }
 }
