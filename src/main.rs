f868f0f7e5f0294676ea6d1ea49865ae7cc29c07
Lucas Balieiro <lucasbalieirom@hotmail.com>
2025-03-05 22:08:19 -0400

    feat: implements telnet chatserver

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
@@ -8,28 +9,40 @@ async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
     println!("Server started at localhost:8080");
 
+    let (tx, mut rx) = broadcast::channel(10);
+
     loop {
         let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
+
             println!("Connection received from {:?}", addr);
             let mut reader = BufReader::new(reader);
 
             loop {
                 let mut line = String::new();
 
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
 
-                if bytes_read == 0 {
-                    break;
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+
+                        if addr != other_addr{
+                            writer.write_all(&msg.as_bytes()).await.unwrap();
+                        }
+
+                    }
                 }
-
-                println!("GOT={}", line);
-
-                writer.write_all(&line.as_bytes()).await.unwrap();
-
-                line.clear();
             }
         });
     }
